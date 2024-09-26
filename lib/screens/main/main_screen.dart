55c794b1ee4709dd4511d093d33da0eccf7cbbb5
abdullah017 <abdullahozvanli@gmail.com>
2024-09-26 19:36:55 +0300

    initial

---
+++ lib/screens/main/main_screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:news/controllers/MenuController.dart';
+import 'package:news/screens/home/home_screen.dart';
+import '../../constants.dart';
+import 'components/header.dart';
+import 'components/side_menu.dart';
+
+class MainScreen extends StatelessWidget {
+  final MyMenuController _controller = Get.put(MyMenuController());
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: _controller.scaffoldkey,
+      drawer: SideMenu(),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Header(),
+            Container(
+              padding: EdgeInsets.all(kDefaultPadding),
+              constraints: BoxConstraints(maxWidth: kMaxWidth),
+              child: SafeArea(child: HomeScreen()),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
